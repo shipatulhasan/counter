9e648fd04a0d5be54bafc6a5c00c902731b93566
shipatulhasan <shipatulhasan328@gmail.com>
2023-03-12 15:21:30 +0600

    updated

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-
-let count = 0
+const getCount = ()=>{
+    let count = 0
+    const storedCount = localStorage.getItem('count')
+    if(storedCount){
+        count = parseInt(storedCount)
+    }
+    return count
+}
 document.getElementById('reset').addEventListener('click',function(){
-    count = 0
+    localStorage.clear()
     display()
     
 })
 document.getElementById('plus').addEventListener('click',function(){ 
-    count++
+    let plusCount = getCount()
+    plusCount++
+    localStorage.setItem('count',plusCount)
     display()
 })
 document.getElementById('minus').addEventListener('click',function(){
-    if(count > 0){
-        count = count - 1
+    let minusCount = getCount()
+    if(minusCount > 0){
+        minusCount = minusCount - 1
     }
+    localStorage.setItem('count',minusCount)
     display()
 })
 
 function display(){
     const displayCounter = document.getElementById('display')
-    displayCounter.innerText = count
+    const displayCount = getCount()
+    console.log(displayCount)
+    displayCounter.innerText = displayCount
 }
+display()
 
 
